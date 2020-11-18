a5c7d5d2b7d0966728aa382c6e72fe39c82e3e03
Tusshar Laddha <32119637+tusshar2000@users.noreply.github.com>
2020-11-18 22:06:42 +0530

    Add files via upload

--- offline.py
+++ offline.py
@@ -1,14 +1,34 @@
+# import glob
+# import os
+# import pickle
+# from PIL import Image
+# from feature_extractor import FeatureExtractor
+
+# fe = FeatureExtractor()
+
+# for img_path in sorted(glob.glob('static/img/*.jpg')):
+#     print(img_path)
+#     img = Image.open(img_path)  # PIL(Python Imaging Library) image
+#     feature = fe.extract(img)
+#     feature_path = 'static/feature/' + os.path.splitext(os.path.basename(img_path))[0] + '.pkl'
+#     pickle.dump(feature, open(feature_path, 'wb'))
+
 import glob
 import os
 import pickle
 from PIL import Image
 from feature_extractor import FeatureExtractor
-
+import numpy as np
 fe = FeatureExtractor()
-
+features=[]
+images=[]
 for img_path in sorted(glob.glob('static/img/*.jpg')):
     print(img_path)
     img = Image.open(img_path)  # PIL(Python Imaging Library) image
     feature = fe.extract(img)
     feature_path = 'static/feature/' + os.path.splitext(os.path.basename(img_path))[0] + '.pkl'
-    pickle.dump(feature, open(feature_path, 'wb'))
+    features.append(feature)
+    print(img_path)
+    images.append(img_path)
+np.save("features.npy",np.array(features))
+np.save("images.npy",np.array(images))
